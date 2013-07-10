8f9952eb322c80e7930f703bfedb895420d3f470
Stewart Duffey <duffey.sj@gmail.com>
2013-07-10 19:26:49 +0100

    Add hardcoded delay to all requests, change backend port to 3000

--- src/main/scala/badproxy/ProxyService.scala
+++ src/main/scala/badproxy/ProxyService.scala
@@ -1,7 +1,8 @@
 package badproxy
 
 import scala.concurrent.duration._
-import akka.pattern.ask
+import scala.concurrent.Future
+import akka.pattern.{ask, after}
 import akka.util.Timeout
 import akka.actor._
 import akka.io.IO
@@ -35,19 +36,25 @@ class ProxyService extends Actor with SprayActorLogging {
         case Some(Host(host, port)) ⇒ log.info("Request has host " + host + " and port " + port + " with path " + req.uri)
       }*/
 
-      val newRequest = updateRequestHostAndPort(req, "localhost", 80)
+      val newRequest = updateRequestHostAndPort(req, "localhost", 3000)
       //log.info("About to ask IO(Http) to make a request")
       //log.info(newRequest.toString)
 
       val client = sender
-      IO(Http).ask(newRequest).mapTo[HttpResponse] onSuccess {
-        case resp: HttpResponse => 
-          //log.info("Got the HttpResponse...")
-          //log.info(resp.toString)
-          client ! resp
-        case _ => 
-          sys.error("BOOM!")
-      }
+      //IO(Http).ask(newRequest).flatMap(r => Future(r)).mapTo[HttpResponse] onSuccess {
+      IO(Http)
+        .ask(newRequest)
+        .flatMap(r => 
+          after(2 seconds, using = system.scheduler)(Future(r))
+        )
+        .mapTo[HttpResponse] onSuccess {
+          case resp: HttpResponse => 
+            //log.info("Got the HttpResponse...")
+            //log.info(resp.toString)
+            client ! resp
+          case _ => 
+            sys.error("BOOM!")
+        }
   }
 
   def updateRequestHostAndPort(req: HttpRequest, host: String, port: Int): HttpRequest = {
@@ -57,7 +64,7 @@ class ProxyService extends Actor with SprayActorLogging {
     val hostWithIndex = req.headers.zipWithIndex.find(_._1.isInstanceOf[Host])
     val newHeaders = hostWithIndex match {
       case Some((_, index: Int)) => 
-        req.headers.updated(index, Host(host, port))
+        req.headers.updated(index, Host(host))
       case _ => 
         req.headers
     }
